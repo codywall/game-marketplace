7c76ae2fcaa00268239c9968a279c5dc144faa27
Rodrigo Espinoza <Rodespinoza@csumb.edu>
2020-04-30 06:39:06 +0000

    finshed up login and create account

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 /* Require external APIs and start our application instance */
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var request = require('request');
+var mysql = require('mysql'); 
+var session = require('express-session');
+var bcrypt = require('bcrypt');
 
+/* Start the application server */
+app.listen(process.env.PORT || 8080, process.env.IP, function () {
+  console.log('Express server is running...');
+});
 
 /* Configure our server to read public folder and ejs files */
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(session({
+  secret:"top secret!",
+  resave: true,
+  saveUnitialized:true
+}));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 /* The handler for the DEFAULT route */
@@ -25,15 +41,50 @@ app.get('/admin', function(req,res){
    res.render('admin');
 });
 
-app.get('/results', function(req, res){
+app.get('/addGame', function(req, res){
 	let game = req.query.search;
 	const url = `https://api.rawg.io/api/games?search=${game}`;
 	request(url, function(error, response, data){
 		if (!error && response.statusCode == 200){
-      let parsedData = JSON.parse(data);
-			res.render('addGame', {games: parsedData.results});
+			data = JSON.parse(data);
+			res.render('addGame', {games: data.results});
 		}
 	});
+});
+
+app.post("/addGame", async function(req, res){
+  let rows = await insertGame(req.body);
+  console.log(rows);
+  let message = "Listing was not added to the database.";
+  if (rows.affectedRows > 0) {
+    message= "Listing successfully added!";
+  }
+  res.render("addGame", {"message":message});
+});
+
+function insertGame(body, group){
+  let conn = dbConnection();
+  return new Promise(function(resolve, reject){
+    conn.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!: insertGame");
+        
+      let sql = `INSERT INTO listings
+                        (title, genre, image_url, price, seller_username)
+                         VALUES ('${body.title}', '${body.genre}', '${body.imageURL}', ${body.price}, '${body.username}')`;
+        
+      conn.query(sql, function (err, rows, fields) {
+              if (err) throw err;
+              conn.end();
+              resolve(rows);
+      });
+      console.log(sql);
+        
+        });
+    });
+}
+app.get('/cart', function(req,res){
+   res.render('cart');
 });
 /* The handler for undefined routes */
 app.get('*', function (req, res) {
@@ -52,7 +103,72 @@ function dbConnection() {
 }
 
 
-/* Start the application server */
-app.listen(process.env.PORT || 8080, process.env.IP, function () {
-  console.log('Express server is running...');
+
+app.post("/createAccount",function (req,res){
+  let username = req.body.username;
+  let password= req.body.password;
+  let name= req.body.firstName;
+  let lastName = req.body.lastName;
+  let repeatPassword = req.body.repeatPassword;
+  let conn = dbConnection();
+  let salt = 10;
+  let count = 5;
+  bcrypt.hash(password,salt,function(error,hash){
+     if(error) throw error;
+     var stmt='INSERT INTO users (first_name ,last_name ,username , password) VALUES (?,?,?,?)';
+     var data = [name,lastName,username,hash];
+     
+     conn.query(stmt,data,function(error,result){
+     if(error) throw error;
+      res.redirect("/login");
+    });
+    
+  });
+  
+  console.log("name "+ name);
+  console.log("lastName "+ lastName);
+  console.log("username "+ username);
+  console.log("password "+password);
+  console.log("repeatPassword "+ repeatPassword);
+
+  
 });
+
+app.post("/login", async function(req,res){
+  let users = await  userexist(req.body.username);
+  let hashedPassword = users.length >0? users[0].password :'';
+  let passwordMatch = await checkPassword(req.body.password,hashedPassword);
+  if(passwordMatch){
+    res.render("index");
+  }else{
+    res.render("login",{error:true});
+  }
+ 
+
+  console.log("username "+ req.body.username);
+  console.log("password "+ req.body.password);
+  console.log(users);
+
+});
+
+function userexist(username){
+  let stmt ='SElECT * FROM users where username=?';
+  let conn = dbConnection();
+  return new Promise(function(resolve,reject){
+    conn.query(stmt,[username],function(error, results) {
+        if(error) throw error;
+        resolve(results);
+    });
+    
+  });
+  
+}
+
+function checkPassword(password,hash){
+  return new Promise (function (resolve,reject){
+    bcrypt.compare(password,hash, function(error,result){
+      if(error) throw error;
+      resolve(result)
+    });
+  });
+}
